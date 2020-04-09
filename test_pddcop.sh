440e183c2fb8f2368f7e03417c215276b1ab8135
Khoi Hoang <khoang@cs.nmsu.edu>
2020-04-09 12:24:30 -0500

    Update executable script

--- test_pddcop.sh
+++ test_pddcop.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
-# alg=rdiff,cdiff,base
-
+alg=(C_DPOP LS_SDPOP LS_RAND FORWARD BACKWARD SDPOP REACT HYBRID)
+dynamics=(FINITE_HORIZON INFINITE_HORIZON ONLINE)
 
 # java -jar build/ND-DCOP-1.0-jar-with-dependencies.jar $LS_RAND rep_0_d5.dzn 10 15 0.9 FINITE_HORIZON 0.3
 topology="random"
 jar_file="target/ND-DCOP-1.0-jar-with-dependencies.jar"
 
-for algorithm in "LS_RAND"
+decisionDomain=3
+randomDomain=3
+
+clear
+killall -9 java
+# for algorithm in LS_RAND LS_SDPOP FORWARD BACKWARD
+for algorithm in C_DPOP
 do
-  for decision in 5
+  for decision in 10
   do
-    for random in 4
+    for random in 5
     do
       for instance in 0
       do
-        folder="random_x"$decision"_y"$random
-        input_file=$folder"/instance_"$instance"_x"$decision"_y"$random".dzn"
-        for horizon in 10
+        folder="random_x"$decision"_y"$random"_dx"$decisionDomain"_dy"$randomDomain
+        input_file=$folder"/instance_"$instance"_x"$decision"_y"$random"_dx"$decisionDomain"_dy"$randomDomain".dzn"
+        for horizon in 1
         do
-          for switching_cost in 15
+          for switching_cost in 1000
           do
-            for discount_factor in 0.9
+            for discount_factor in 0.7
             do
               for dynamic_type in FINITE_HORIZON
               do
-                for heuristic_weight in 0.3
+                for heuristic_weight in 0
                 do
                   java -jar $jar_file $algorithm $input_file $horizon $switching_cost $discount_factor $dynamic_type $heuristic_weight
+                  killall -9 java
+                  sleep 1s
                 done
               done
             done
